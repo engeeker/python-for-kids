6acbc385875584f2485d0e8a0d4e4e88ae6d76f5
Peng Xiao <xiaoquwl@gmail.com>
2019-10-23 21:37:20 +0200

    test

---
+++ game.py
@@ -0,0 +1,55 @@
+import pygame        # Setup
+pygame.init()
+screen = pygame.display.set_mode([800,600])
+keep_going = True
+pic = pygame.image.load("flight.png")
+picx = 0
+picy = 0
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+paddlew = 200
+paddleh = 90
+paddlex = 300
+paddley = 550
+points = 0
+picw = 100
+pich = 150
+lives = 5
+font = pygame.font.SysFont("Times", 24)
+timer = pygame.time.Clock()
+speedx = 10
+speedy = 10
+while keep_going:    # Game loop
+    if lives == 4:
+        pygame.quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keep_going = False
+    picx = picx + speedx
+    picy += speedy
+    if picx <= 0 or picx + pic.get_width() >= 800:
+        speedx = -speedx
+    if picy <= 0:
+        speedy = -speedy
+    if picy >= 500:
+        lives -= 1
+        speedy = -speedy
+    screen.fill(BLACK)
+    screen.blit(pic, (picx, picy))
+    paddlex = pygame.mouse.get_pos()[0]
+    paddlex -= paddlew/2
+    pygame.draw.rect(screen, WHITE, (paddlex, paddley, paddlew, paddleh))
+    # Check for paddle bounce
+    if picy + pich >= paddley and picy + pich <= paddley + paddleh and speedy > 0:
+        if picx + picw / 2 >= paddlex and picx + picw / 2 <= paddlex +  paddlew:
+            points += 1
+            speedy = -speedy
+    draw_string = "Lives: " + str(lives) + " Points: " + str(points)
+    text = font.render(draw_string, True, WHITE)
+    text_rect = text.get_rect()
+    text_rect.centerx = screen.get_rect().centerx
+    text_rect.y = 10
+    screen.blit(text, text_rect)
+    pygame.display.update()
+    timer.tick(60)
+pygame.quit()
