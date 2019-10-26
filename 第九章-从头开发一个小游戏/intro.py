6212636a5cfa0afa60412728c3444ab464ac4473
Peng Xiao <xiaoquwl@gmail.com>
2019-10-26 18:49:26 +0200

    test
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ 第九章-从头开发一个小游戏/intro.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([800,600])
+# keep_going = True
+# pic = pygame.image.load("smile.png")
+# picx = 0
+# picy = 0
+# BLACK = (0,0,0)
+
+# while keep_going:
+
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             keep_going = False
+    
+#     screen.fill(BLACK)
+#     screen.blit(pic, (picx, picy))
+
+#     pygame.display.update()
+# pygame.quit()
